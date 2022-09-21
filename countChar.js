65b985449ad3d51f1788c7e5f0334ffabb4a175d
dipenmotka <dipen@atharvasystem.com>
2022-09-21 11:45:16 +0530

    optimized by removing if else and charAt

--- countChar.js
+++ countChar.js
@@ -1,25 +1,19 @@
-function duplicateCount(str) {
+function duplicateCount (str) {
+  var final = '';
+  // var strLength = str?.length;
+  // if (strLength) {
+  //   console.log(str.length)  ;
+  // }
+  for (let i = 0; i < str.length; i++) {
+    var count = 1;
 
-	if(str.length > 0 ) {
-
-		var final = "";
-
-		for(let i = 0; i < str.length; i++) {
-			var count = 1 ;
-
-			while (i+1 < str.length && str.charAt(i) == str.charAt(i + 1)) {
-				i++;
-				count++;
-			}
-			if ( count == 1 ) {
-				final += str.charAt(i) ;
-			} else {
-				final += str.charAt(i) + count ;
-			}
-		}
-	console.log(final);
-	}		
+    while (i + 1 < str.length && str[i] == str[i + 1]) {
+      i++;
+      count++;
+    }
+    count == 1 ? final += str[i] : final += str[i] + count ;
+  }
+  console.log(final);
 }
 
-duplicateCount("AAABCCCFDAA");
-
+duplicateCount("aee");
