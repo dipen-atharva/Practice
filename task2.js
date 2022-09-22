5ab81450d6bc73a787fb87efe1a6a054374b6103
dipenmotka <dipen@atharvasystem.com>
2022-09-22 19:46:53 +0530

    optimizing hash task

--- task2.js
+++ task2.js
@@ -1,4 +1,23 @@
-let input ={
-    classA : [1,2,3],
-    classB : [4,5,6]
+let input = {
+  classA: [1, 2, 3],
+  classB :[4, 5, 6]
 }
+
+let output = {};
+
+// output[Object.values(input)[0][0]] = Object.keys(input)[0] ;
+
+// Object.keys(input).forEach(key => {
+  for (const key in input) {
+    console.log(key);
+    Object.values(input).forEach(val => {
+      
+      val.forEach(char => {
+        output[char] = key;
+console.log(output);
+
+      });
+    });
+  }
+// });
+console.log(output);
