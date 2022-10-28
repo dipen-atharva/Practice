aa5a530b1ef5590f86043c1db0b199a5c79c99cc
Dipen <dipenmotka@gmail.com>
2022-10-28 18:11:01 +0530

    prototype  with writable

--- js3/proto.js
+++ js3/proto.js
@@ -1,11 +1,37 @@
 let animal = {
-    eats: true
-};
-let rabbit = {
-    jumps: true
+    eats: true,
+    walk() {
+        console.log("Animal Walk");
+    },
+    run() {
+        console.log("This method won't be used by rabbit");
+    }
 };
 
-rabbit.__proto__ = animal;
-console.log(rabbit.eats); // true 
+let rabbit = {
+    jumps: true,
+    __proto__: animal
+};
+
+// rabbit.__proto__ = animal;
+// console.log(rabbit.eats); // true 
 console.log(rabbit.jumps); // true
 
+rabbit.walk();
+
+// Writing does not use prototype
+rabbit.run = function () {
+    console.log('Rabbit run');
+}
+
+rabbit.run();
+
+for (let prop in rabbit) {
+    let isOwn = rabbit.hasOwnProperty(prop);
+
+    if (isOwn) {
+        console.log(`Our: ${prop}`);
+    } else {
+        console.log(`Inherited: ${prop}`); 
+    }
+}
