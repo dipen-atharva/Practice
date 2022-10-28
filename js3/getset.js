09c7736821aa57efbd065ef4ed6aceb8e32928d8
Dipen <dipenmotka@gmail.com>
2022-10-28 16:39:11 +0530

    get set with condition

--- js3/getset.js
+++ js3/getset.js
@@ -1,18 +1,38 @@
-let user = {
-    name: "John",
-    surname: "Smith",
+// let user = {
+//     name: "John",
+//     surname: "Smith",
 
-    get fullName() {
-        return `${this.name} ${this.surname}`;
+//     get fullName() {
+//         return `${this.name} ${this.surname}`;
+//     },
+
+//     set fullName(value) {
+//         [this.name, this.surname] = value.split(" ");
+//     }
+// };
+
+// // set fullName is executed with the given value.
+// user.fullName = "Alice Cooper";
+
+// console.log(user.name); // Alice
+// console.log(user.surname); // Cooper
+
+
+let user = {
+    get name() {
+        return this._name;
     },
 
-    set fullName(value) {
-        [this.name, this.surname] = value.split(" ");
+    set name(value) {
+        if (value.length < 4) {
+            console.log("Name is too short, need at least 4 characters");
+            return;
+        }
+        this._name = value;
     }
 };
 
-// set fullName is executed with the given value.
-user.fullName = "Alice Cooper";
+user.name = "Pete";
+console.log(user.name); // Pete
 
-console.log(user.name); // Alice
-console.log(user.surname); // Cooper
+user.name = ""; 
